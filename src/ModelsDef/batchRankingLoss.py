9e75d8139b2b5302f94bd1a211dcb9407e21e071
Luis-Domenech <luis.domenech1@upr.edu>
2019-04-03 14:51:02 -0400

    Added differentiable functions

--- src/ModelsDef/batchRankingLoss.py
+++ src/ModelsDef/batchRankingLoss.py
@@ -1,25 +1,64 @@
-import tensorflow as tf
+from keras import backend as K
 
-def brLoss(batch):    
+def brLoss(batch):
+    
+    def greaterThanApprox(x, y):
+        epsilon = 1E-8
+        abse = np.sqrt((x - y)**2 + epsilon)
+        return 0.5 * (x + y + abse)
 
+    def piecewiseFunction1(x, y):
+        epsilon = 1E-8
+        threshold = 1E-1
+        approx = greaterThanApprox(x, y)
+
+        check = approx - (x * (1 + threshold))
+
+        #For checking if x == y
+        same = x - y
+        sameFactor = (same / (same + epsilon))
+
+        value = np.abs(check) / check 
+        value -= (2 * (sameFactor - 1))
+
+        return value
+
+    def piecewiseFunction2(x, y):
+        epsilon = 1E-8
+        threshold = 1E-1
+        approx = greaterThanApprox(x, y)
+
+        check = approx - (x * (1 + threshold))
+
+        #For checking if x == y
+        same = x - y
+        sameFactor = same / (same + epsilon)
+
+        value = (check - np.abs(check)) / ((check - np.abs(check)) + epsilon)
+        value *= sameFactor
+
+        return value
+    
     def batchRankingLoss(y_true, y_pred):
         """ Final loss calculation function to be passed to optimizer"""
-        L = 0.0
-        y_true = tf.reshape(y_true, [-1])
-        y_pred = tf.reshape(y_pred, [-1])
+        L = y_pred[0,0] * 0.0
+        #y_true = K.tf.reshape(y_true, [-1])
+        #y_pred = K.tf.reshape(y_pred, [-1])
+
         for i in range(batch):
             for j in range(batch):
                 if i != j:
-                    y_ij = 0
+                    
+                    #y_ij = K.tf.cond(K.tf.greater(y_true[i], y_true[j]), lambda: K.tf.constant(-1.0), lambda: K.tf.constant(1.0))
+                    y_ij = piecewiseFunction1(y_true[i,0], y_true[j,0])
 
-                    y_ij = tf.cond(tf.greater(y_true[i], y_true[j]), lambda: tf.constant(-1.0), lambda: tf.constant(1.0))
+                    #w_ij = K.tf.cond(K.tf.greater(K.tf.abs(y_true[i] - y_true[j]), K.tf.constant(0.1)), lambda: K.tf.constant(1.0), lambda: K.tf.constant(0.0))
+                    w_ij = piecewiseFunction2(K.tf.abs(y_true[i,0] - y_true[j,0]), K.tf.constant(0.1))
 
-                    w_ij = tf.cond(tf.greater(tf.abs(y_true[i] - y_true[j]), tf.constant(0.1)), lambda: tf.constant(1.0), lambda: tf.constant(0.0))
-
-                    L_ij = w_ij * (tf.reduce_max(tf.stack([tf.constant(0.0), (1.0 - y_ij) * (y_pred[i] - y_pred[j])])))
-
+                    L_ij = w_ij * (K.tf.reduce_max(K.tf.stack([0.0, (1.0 - y_ij) * (y_pred[i,0] - y_pred[j,0])])))
+                    #print(L_ij.shape)
                     L += L_ij
 
-        return (1/(batch**2)) * L
+        return K.tf.multiply(1.0/(batch*batch), K.tf.reduce_sum(L))
 
     return batchRankingLoss
