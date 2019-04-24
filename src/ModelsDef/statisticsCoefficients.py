55ef20bc3b53cea881e251448f9c67f54c0dfe25
Luis-Domenech <luis.domenech1@upr.edu>
2019-04-24 15:25:56 -0400

    Added correlation mettrics

---
+++ src/ModelsDef/statisticsCoefficients.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+from scipy.stats import spearmanr, kendalltau
+
+def Pearson(y_true, y_pred):
+
+    return tfp.stats.correlation(y_pred, y_true, event_axis=None)
+
+def Spearman(y_true, y_pred):
+    return (tf.py_function(spearmanr, [tf.cast(y_pred, tf.float32), tf.cast(y_true, tf.float32)], Tout = tf.float32) )
+
+def Kendall(y_true, y_pred):
+    return (tf.py_function(kendalltau, [tf.cast(y_pred, tf.float32), tf.cast(y_true, tf.float32)], Tout = tf.float32) )
+
+    
