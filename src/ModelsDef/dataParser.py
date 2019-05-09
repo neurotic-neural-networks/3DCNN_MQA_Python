ed281574240e3c1c968a30f28fb10f85a8b5ba2e
Luis-Domenech <luis.domenech1@upr.edu>
2019-05-09 02:41:06 -0400

    Moved functions inside protein class

--- src/ModelsDef/dataParser.py
+++ src/ModelsDef/dataParser.py
@@ -54,70 +54,70 @@ class Protein:
             xyz = np.zeros((width, height, depth, layers))
             for atom in decoy["atoms"][0]:
                 #TODO Fix placement
-                xyz[int((atom["coordinates"][0])%xyz.shape[0]), int((atom["coordinates"][1])%xyz.shape[1]), int((atom["coordinates"][2])%xyz.shape[0]), get_layer(atom)] = get_density(atom)
+                xyz[int((atom["coordinates"][0])%xyz.shape[0]), int((atom["coordinates"][1])%xyz.shape[1]), int((atom["coordinates"][2])%xyz.shape[0]), self.get_layer(atom)] = self.get_density(atom)
             coords.append(xyz)
         return coords, scores
 
 
-def get_layer(atom):
-    #HARD COPY FROM PAPER REPO
-    if atom["chain_type"] == "O":
-        if atom["terminal"]:
+    def get_layer(self, atom):
+        #HARD COPY FROM PAPER REPO
+        if atom["chain_type"] == "O":
+            if atom["terminal"]:
+                return 8
+            else:
+                return 6
+        elif atom["chain_type"] == "OXT" and atom["terminal"]:
             return 8
-        else:
-            return 6
-    elif atom["chain_type"] == "OXT" and atom["terminal"]:
-        return 8
-    elif atom["chain_type"] == "OT2" and atom["terminal"]:
-        return 8
-    elif atom["chain_type"] == "N":
-        return 2
-    elif atom["chain_type"] == "C":
-        return 9
-    elif atom["chain_type"] == "CA":
-        return 11
-    else:
-        fullName = atom["residue"] + atom["chain_type"]
-
-        if fullName == "CYSSG" or fullName == "METSD" or fullName == "MSESE":
-            return 1
-        elif fullName == "ASNND2" or fullName == "GLNNE2":
+        elif atom["chain_type"] == "OT2" and atom["terminal"]:
+            return 8
+        elif atom["chain_type"] == "N":
             return 2
-        elif fullName == "HISND1" or fullName == "HISND2" or fullName == "TRPNE1":
-            return 3
-        elif fullName == "ARGNH1" or fullName == "ARGNH2" or fullName == "ARGNE":
-            return 4
-        elif fullName == "LYSNZ":
-            return 5
-        elif fullName == "ACEO" or fullName == "ASDNOD1" or fullName == "GLNOE1":
-            return 6
-        elif fullName == "SEROG" or fullName == "THROG1" or fullName == "TYROH":
-            return 7
-        elif fullName == "ASPOD1" or fullName == "ASPOD2" or fullName == "GLUOE1" or fullName == "GLUOE2":
-            return 8
-        elif fullName == "ARGCZ" or fullName == "ASPCG" or fullName == "GLUCD" or fullName == "ACEC" or fullName == "ASNCG" or fullName == "GLNCD":
+        elif atom["chain_type"] == "C":
             return 9
-        elif fullName == "HISCD2" or fullName == "HISCE1" or fullName == "HISCG" or fullName == "PHECD1" or fullName == "PHECD2" or fullName == "PHECE1" or fullName == "PHECE2" or fullName == "PHECG" or fullName == "PHECZ" or fullName == "TRPCD1" or fullName == "TRPCD2" or fullName == "TRPCE2" or fullName == "TRPCE3" or fullName == "TRPCG" or fullName == "TRPCH2" or fullName == "TRPCZ2" or fullName == "TRPCZ3" or fullName == "TYRCD1" or fullName == "TYRCD2" or fullName == "TYRCE1" or fullName == "TYRCE2" or fullName == "TYRCG" or fullName == "TYRCZ":
-            return 10
-        elif fullName == "ALACB" or fullName == "ARGCB" or fullName == "ARGCG" or fullName == "ARGCD" or fullName == "ASNCB" or fullName == "ASPCB" or fullName == "GLNCB" or fullName == "GLNCG" or fullName == "GLUCB" or fullName == "GLUCG" or fullName == "HISCB" or fullName == "ILECB" or fullName == "ILECD1" or fullName == "ILECG1" or fullName == "ILECG2" or fullName == "LEUCB" or fullName == "LEUCD1" or fullName == "LEUCD2" or fullName == "LEUCG" or fullName == "LYSCB" or fullName == "LYSCD" or fullName == "LYSCG" or fullName == "LYSCE" or fullName == "METCB" or fullName == "METCE" or fullName == "METCG" or fullName == "MSECB" or fullName == "MSECE" or fullName == "MSECG" or fullName == "PHECB" or fullName == "PROCB" or fullName == "PROCG" or fullName == "PROCD" or fullName == "SERCB" or fullName == "THRCG2" or fullName == "TYRCB" or fullName == "VALCB" or fullName == "VALCG1" or fullName == "VALCG2" or fullName == "ACECH3" or fullName == "THRCB" or fullName == "CYSCB":
+        elif atom["chain_type"] == "CA":
             return 11
         else:
-            return 0
-           #print("Invalid atom: " + fullName)
-           #raise Exception("LINE DATA ERROR")
+            fullName = atom["residue"] + atom["chain_type"]
+
+            if fullName == "CYSSG" or fullName == "METSD" or fullName == "MSESE":
+                return 1
+            elif fullName == "ASNND2" or fullName == "GLNNE2":
+                return 2
+            elif fullName == "HISND1" or fullName == "HISND2" or fullName == "TRPNE1":
+                return 3
+            elif fullName == "ARGNH1" or fullName == "ARGNH2" or fullName == "ARGNE":
+                return 4
+            elif fullName == "LYSNZ":
+                return 5
+            elif fullName == "ACEO" or fullName == "ASDNOD1" or fullName == "GLNOE1":
+                return 6
+            elif fullName == "SEROG" or fullName == "THROG1" or fullName == "TYROH":
+                return 7
+            elif fullName == "ASPOD1" or fullName == "ASPOD2" or fullName == "GLUOE1" or fullName == "GLUOE2":
+                return 8
+            elif fullName == "ARGCZ" or fullName == "ASPCG" or fullName == "GLUCD" or fullName == "ACEC" or fullName == "ASNCG" or fullName == "GLNCD":
+                return 9
+            elif fullName == "HISCD2" or fullName == "HISCE1" or fullName == "HISCG" or fullName == "PHECD1" or fullName == "PHECD2" or fullName == "PHECE1" or fullName == "PHECE2" or fullName == "PHECG" or fullName == "PHECZ" or fullName == "TRPCD1" or fullName == "TRPCD2" or fullName == "TRPCE2" or fullName == "TRPCE3" or fullName == "TRPCG" or fullName == "TRPCH2" or fullName == "TRPCZ2" or fullName == "TRPCZ3" or fullName == "TYRCD1" or fullName == "TYRCD2" or fullName == "TYRCE1" or fullName == "TYRCE2" or fullName == "TYRCG" or fullName == "TYRCZ":
+                return 10
+            elif fullName == "ALACB" or fullName == "ARGCB" or fullName == "ARGCG" or fullName == "ARGCD" or fullName == "ASNCB" or fullName == "ASPCB" or fullName == "GLNCB" or fullName == "GLNCG" or fullName == "GLUCB" or fullName == "GLUCG" or fullName == "HISCB" or fullName == "ILECB" or fullName == "ILECD1" or fullName == "ILECG1" or fullName == "ILECG2" or fullName == "LEUCB" or fullName == "LEUCD1" or fullName == "LEUCD2" or fullName == "LEUCG" or fullName == "LYSCB" or fullName == "LYSCD" or fullName == "LYSCG" or fullName == "LYSCE" or fullName == "METCB" or fullName == "METCE" or fullName == "METCG" or fullName == "MSECB" or fullName == "MSECE" or fullName == "MSECG" or fullName == "PHECB" or fullName == "PROCB" or fullName == "PROCG" or fullName == "PROCD" or fullName == "SERCB" or fullName == "THRCG2" or fullName == "TYRCB" or fullName == "VALCB" or fullName == "VALCG1" or fullName == "VALCG2" or fullName == "ACECH3" or fullName == "THRCB" or fullName == "CYSCB":
+                return 11
+            else:
+                return 0
+            #print("Invalid atom: " + fullName)
+            #raise Exception("LINE DATA ERROR")
 
 
-def get_density(atom):
-    van_der_waal_radii = {'H' : 1.2, 'C' : 1.7, 'N' : 1.55, 'O' : 1.52,
-    'S' : 1.8, 'D' : 1.2, 'F' : 1.47, 'CL' : 1.75, 'BR' : 1.85, 'P' : 1.8,
-    'I' : 1.98, 'E' : 1.0, 'X':1.0 , '': 0.0}
-    # Source:https://physlab.lums.edu.pk/images/f/f6/Franck_ref2.pdf
+    def get_density(self, atom):
+        van_der_waal_radii = {'H' : 1.2, 'C' : 1.7, 'N' : 1.55, 'O' : 1.52,
+        'S' : 1.8, 'D' : 1.2, 'F' : 1.47, 'CL' : 1.75, 'BR' : 1.85, 'P' : 1.8,
+        'I' : 1.98, 'E' : 1.0, 'X':1.0 , '': 0.0}
+        # Source:https://physlab.lums.edu.pk/images/f/f6/Franck_ref2.pdf
 
-    if atom["chain_type_single"] not in van_der_waal_radii:
-        print("Invalid atom: " + atom["chain_type_single"])
-        raise Exception("LINE DATA ERROR")
+        if atom["chain_type_single"] not in van_der_waal_radii:
+            print("Invalid atom: " + atom["chain_type_single"])
+            raise Exception("LINE DATA ERROR")
 
-    return pow(math.e, -1 * pow(van_der_waal_radii[atom["chain_type_single"]], 2)/2)
+        return pow(math.e, -1 * pow(van_der_waal_radii[atom["chain_type_single"]], 2)/2)
 
 
 def readAndStoreData(image_dir):
@@ -318,12 +318,6 @@ def readAndStoreData(image_dir):
     with open('all_protein_decoy_data.pickle', 'wb') as f:
         pickle.dump(proteins, f, -1)
 
-def loadData():
-    #TODO implement this with dignity >:l
-    readAndStoreData("C:\\Users\\Owrn\\Documents\\gitRepos\\3DCNN_MQA_Python\\src\\ModelsDef\\CASP11Stage1_SCWRL")
-    proteins = pickle.load(open("all_protein_decoy_data.pickle", "rb", -1))
-
-    return ([proteins[1].getData()[0]], proteins[1].getData()[1]), ([proteins[2].getData()[0]], proteins[2].getData()[1])
 
 if __name__ == "__main__":
     # Here we call our awesome parser :)
